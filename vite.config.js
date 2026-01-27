27790362395afd866fd8be0877f94875427c4386
souleymane1234 <konatesouleymane42@gmail.com>
2026-01-27 10:42:42 +0000

    corrige

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    server: {
+  server: {
     port: 3000, // choisis ton port ici
     host: true  // optionnel : permet l'accès depuis le réseau local
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'mui-vendor': ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+          'query-vendor': ['@tanstack/react-query'],
+        }
+      }
+    }
   }
 })
 
